c2be6c8832dee7d2ed56df401287ae0167e0f83a
RomanBaryshevOG <romanbaryshev.y@gmail.com>
2022-08-10 17:09:05 +0500

    Fix Task0

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Напишите программу, которая на вход принимает число и выдает его квадрат.
 
-Console.WriteLine("Введите целое число: ");
+Console.Write("Введите целое число: ");
 int num = Convert.ToInt32(Console.ReadLine());
 int square = num * num;
 Console.Write($"Квадрат числа {num} = {square}"); // $ - интерполяция строк
