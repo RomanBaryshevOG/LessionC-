663d692465488b20d5654125c854cae65c914821
RomanBaryshevOG <romanbaryshev.y@gmail.com>
2022-08-11 15:31:22 +0500

    Fix

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -7,7 +7,7 @@ int number = new Random().Next(10, 100);
 int firstDigit = number / 10;
 int secondDigit = number % 10;
 
-if (firstDigit == secondDigit) Console.WriteLine("Цифры равны ");
+if (firstDigit == secondDigit) Console.WriteLine($"Цифры равны = {number} ");
 else if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {number} =  {firstDigit}");
 else Console.WriteLine($"Наибольшая цифра числа {number} = {secondDigit}");
 
