74c1a908a2c4e316437b8a7b2dd25ff2ee4170d1
RomanBaryshevOG <romanbaryshev.y@gmail.com>
2022-08-15 23:45:22 +0500

    Add Task16

---
+++ Task16/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите задачу которая принимает на вход число N и 
+// выдает произведение чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int num1 = 1;
+for (int i = 1; i <= num; i++)
+{
+    num1 = num1 * i;
+}
+Console.WriteLine(num1);
