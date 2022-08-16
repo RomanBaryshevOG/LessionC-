bc070653f038f435bf27203da914e5b7c2ed10d0
RomanBaryshevOG <romanbaryshev.y@gmail.com>
2022-08-16 15:05:00 +0500

    Add Task15

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -7,4 +7,14 @@
 // Console.WriteLine("Введите число: ")
 // int num = Convert.ToInt32(Console.ReadLine());
 
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int i = 0;
+int input = num;
+while (num > 0)
+{
+    num /= 10;
+    i++;
+}
 
+Console.WriteLine($"Число состоит из {i} цифр");
