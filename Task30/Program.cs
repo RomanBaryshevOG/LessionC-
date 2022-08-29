897a444654e87422b74b1b369a0994b894eb4c73
RomanBaryshevOG <romanbaryshev.y@gmail.com>
2022-08-29 19:35:04 +0500

    Add Task30

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -6,4 +6,49 @@
 // 1 2 3 4
 // 2 3 4 5
 
+int[,] CreateMatrixRndInt(int row, int col, int min, int max) // создаем метод 
+{
+    int[,] matrix = new int[row, col];
+    Random rnd = new Random();
 
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max +1);
+        }
+    }
+    return matrix;
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {   
+        Console.Write("[");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(j < matrix.GetLength(1) -1 ?
+            $"{matrix[i, j], 4}" : $"{matrix[i, j], 4}");
+        }
+        Console.WriteLine("]");
+    }
+}
+
+void MatrixSumIDx(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = i + j;
+        }
+    }
+    
+}
+
+int[,] array2D = CreateMatrixRndInt(3, 4, -9, 9); // возвращает массив, создали переменную array2D
+PrintMatrix(array2D);
+Console.WriteLine();
+MatrixSumIDx(array2D);
+PrintMatrix(array2D);
