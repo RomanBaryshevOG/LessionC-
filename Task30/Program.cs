97c0f5b5cc90b6d69cc52c091e8d1cf51a30db3f
RomanBaryshevOG <romanbaryshev.y@gmail.com>
2022-08-29 19:47:04 +0500

    fixx

--- Task30/Program.cs
+++ Task30/Program.cs
@@ -6,7 +6,7 @@
 // 1 2 3 4
 // 2 3 4 5
 
-int[,] CreateMatrixRndInt(int row, int col, int min, int max) // создаем метод 
+int[,] CreateMatrixRndInt(int row, int col, int min, int max) // создаем метод для создания массива исхожного
 {
     int[,] matrix = new int[row, col];
     Random rnd = new Random();
@@ -21,7 +21,7 @@ int[,] CreateMatrixRndInt(int row, int col, int min, int max) // создаем 
     return matrix;
 }
 
-void PrintMatrix(int[,] matrix)
+void PrintMatrix(int[,] matrix) // Содаем метод который печатает массив
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {   
@@ -35,13 +35,13 @@ void PrintMatrix(int[,] matrix)
     }
 }
 
-void MatrixSumIDx(int[,] matrix)
+void MatrixSumIDx(int[,] matrix) // в качестве параметра передаем в метод массив
 {
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    for (int i = 0; i < matrix.GetLength(0); i++) // проходим по строкам
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        for (int j = 0; j < matrix.GetLength(1); j++) // проходим по столбцам
         {
-            matrix[i, j] = i + j;
+            matrix[i, j] = i + j; // формула из задачи
         }
     }
     
@@ -49,6 +49,6 @@ void MatrixSumIDx(int[,] matrix)
 
 int[,] array2D = CreateMatrixRndInt(3, 4, -9, 9); // возвращает массив, создали переменную array2D
 PrintMatrix(array2D);
-Console.WriteLine();
+Console.WriteLine(); // для пробела между массивами в резльтате
 MatrixSumIDx(array2D);
 PrintMatrix(array2D);
