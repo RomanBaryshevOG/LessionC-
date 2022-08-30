c9e0d85cbf1389a05861f19376e5453000ad5c76
RomanBaryshevOG <romanbaryshev.y@gmail.com>
2022-08-30 19:18:22 +0500

    Fix

--- Task28/Program.cs
+++ Task28/Program.cs
@@ -10,13 +10,3 @@ int[] ReturnArray(int[] array)
     return arr;
 }
 
-int[] arr1 = CreateArray(10, 1, 20);
-PrintArray(arr1);
-int[] arr2 = ReturnArray(arr1);
-PrintArray(arr2);
-
-Console.WriteLine("Изменим втрое значение в массиве, чтобы определить скопированный массив");
-
-arr1[1] = 30;
-PrintArray(arr1);
-PrintArray(arr2);
